ff81a5f39657cdc9d3a070be44e72ed6a0299b61
Sean Harrington <miles9080@gmail.com>
2021-06-18 15:55:05 -0700

    date/time
    can read date/time

--- Python_test.py
+++ Python_test.py
@@ -1,8 +1,17 @@
+# Initial print to check run in console
+print("Program Runs")
+
+import time 
+from datetime import datetime
 # Python Timer Test
 def main():
    
-    Test = time.gmtime(0)
-    print(Test)
+    Test = time.time()
+    timestamp = Test
+    dt_object = datetime.fromtimestamp(timestamp)
 
+    print("dt_object = " , dt_object)
+    print("type(dt_object) = ", type(dt_object))
 if __name__ == "__main__":
-    main()
+   main()
+
