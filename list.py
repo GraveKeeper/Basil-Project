31939559ec08373b8ebdeb8fd17c5e0b292fdc9e
Sean Harrington <miles9080@gmail.com>
2021-06-23 01:04:54 -0700

    playing around with classes and stuff
    hard to explain will make read me

---
+++ list.py
@@ -0,0 +1,8 @@
+class TestData:
+	#list test data
+	def check1():
+		print("first checkpoint")
+	def TimeCheck():
+		if TimeCheck == true: print("TimeCheck true")
+	def PrintCheck():
+	def ActionCheck():
